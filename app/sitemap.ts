9bdc21b76aa4a7ae6a0b7556efd9dd3a76ed7871
Martin Gilde <khaolakcompanion@outlook.de>
2026-06-20 09:58:28 +0200

    Sitemap hinzufügen fix

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,7 +4,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: "https://khaolak.app",
-      lastModified: new Date(),
+      lastModified: "2026-06-20",
       changeFrequency: "weekly",
       priority: 1,
     },
