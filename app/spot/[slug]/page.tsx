525481830b17038ff4244cdbe6accb12871325d5
Martin Gilde <khaolakcompanion@outlook.de>
2026-06-21 14:20:53 +0200

    entdecken bauen

--- app/spot/[slug]/page.tsx
+++ app/spot/[slug]/page.tsx
@@ -1,14 +1,18 @@
-import { supabaseServer } from "@/src/lib/supabase-server";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
 
 export default async function SpotPage({ params }: any) {
-
   const slug = params.slug?.trim();
 
-  const { data: spot, error } = await supabaseServer
+  const { data: spot, error } = await supabase
     .from("spots")
     .select("*")
     .eq("slug", slug)
-    .single();
+    .maybeSingle();
 
   if (!spot) {
     return <div>Not found</div>;
